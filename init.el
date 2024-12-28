d71c06f24db82a5cfbafda70a7aa7fc3c12980a9
OverbearingPearl <OverbearingPearl@outlook.com>
2024-12-28 20:14:32 +0800

    feat: make a function to check executable with min-version

--- init.el
+++ init.el
@@ -2,6 +2,22 @@
   (if (file-exists-p custom-file)
       (load custom-file nil :noerror)))
 
+(defun check-executable (my-executable &optional min-version my-executable-path)
+  (defun extract-version-from-string (version-string)
+    (let ((version ""))
+      (when (string-match "\\`[^0-9]*\\([0-9]+[.][0-9]+[.][0-9]+\\(?:[0-9]+\\)?\\).*" version-string)
+        (setq version (match-string 1 version-string)))
+      version))
+  (let* ((executable (or my-executable-path (executable-find my-executable))))
+    (if executable
+        (let* ((version-command (format "%s --version" (shell-quote-argument executable)))
+               (version-string (shell-command-to-string version-command))
+               (version (extract-version-from-string version-string)))
+          (if min-version
+              (version<= min-version version)
+            t))
+      nil)))
+
 (load-file-when-exists "~/.emacs.d/custom/custom-prelude.el")
 
 (setq custom-file "~/.emacs.d/custom/custom.el")
@@ -56,7 +72,7 @@
   :ensure t)
 
 (use-package magit
-  :when (executable-find "git")
+  :when (check-executable "git")
   :ensure t
   :defer t)
 
@@ -66,7 +82,7 @@
   :hook (after-init . global-git-gutter-mode))
 
 (use-package wakatime-mode
-  :when (executable-find "wakatime")
+  :when (check-executable "wakatime")
   :ensure t
   :diminish wakatime-mode
   :hook (after-init . global-wakatime-mode))

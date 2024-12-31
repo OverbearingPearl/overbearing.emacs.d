9484437efea7fba3fba8b0480361107e36c05d9e
OverbearingPearl <OverbearingPearl@outlook.com>
2025-01-01 01:03:34 +0800

    fix: use UTF-8 coding system for keyboard and terminal

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
 
 (defun load-file-when-exists (custom-file)
